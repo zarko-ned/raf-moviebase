7e1e53abda1704bf1b8c5211b274669e4a3dcafc
Zarko <zarkoned@outloo.com>
2024-08-07 11:31:05 +0200

    added adding imdb id on keyboard

--- src/main/java/com/example/imdb/main/Main.java
+++ src/main/java/com/example/imdb/main/Main.java
@@ -5,10 +5,14 @@ import com.example.imdb.exception.MovieNotFoundException;
 import com.example.imdb.model.Movie;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String imdbId = "bla";  // Primer IMDb ID
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter IMDb ID: ");
+        String imdbId = scanner.nextLine();  // Unos IMDb ID-a
+
         IMDbAPIClient client = new IMDbAPIClient();
         try {
             Movie movie = client.fetchMovieData(imdbId);
